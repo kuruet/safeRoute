a6aab344e628d0c65e1a1f6a5cc57d212d2a78bc
kuruet <bpratik728@gmail.com>
2026-03-05 18:24:13 +0530

    initial project commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,8 +32,8 @@ app.use("/api/reports", reportRoutes);
 
 /* Start Server */
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-  console.log(`🚀 SafeRoute server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
